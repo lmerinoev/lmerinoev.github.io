4fab5a7101311029280f177fa7b4a982043e3ed9
lmerinoev <117300887+lmerinoev@users.noreply.github.com>
2026-07-04 09:30:19 -0500

    Habitat: liquid nav drop, daily release count, month trend charts (#44)

--- hearth/sw.js
+++ hearth/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hearth-v16';
+const CACHE = 'hearth-v17';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
